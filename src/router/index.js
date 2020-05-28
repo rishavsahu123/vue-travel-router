0e510a024290c99273e9467cd8efc75f7bc8558d
rishavsahu123 <rishavsahu123@gmail.com>
2020-05-28 08:51:22 +0530

    refactor(code splitting): added chunk and lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "@/views/Brazil.vue";
-import Hawaii from "@/views/Hawaii.vue";
-import Panama from "@/views/Panama.vue";
-import Jamaica from "@/views/Jamaica.vue";
 
 Vue.use(VueRouter);
 
@@ -23,25 +19,30 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
+
   {
     path: "/brazil",
     name: "brazil",
-    component: Brazil
+    component: () =>
+      import(/* webpackChunkName: "brazil"*/ "@/views/Brazil.vue")
   },
   {
     path: "/panama",
     name: "panama",
-    component: Panama
+    component: () =>
+      import(/* webpackChunkName: "panama"*/ "@/views/Panama.vue")
   },
   {
     path: "/jamaica",
     name: "jamaica",
-    component: Jamaica
+    component: () =>
+      import(/* webpackChunkName: "jamaica"*/ "@/views/Jamaica.vue")
   },
   {
     path: "/hawaii",
     name: "hawaii",
-    component: Hawaii
+    component: () =>
+      import(/* webpackChunkName: "hawaii"*/ "@/views/Hawaii.vue")
   }
 ];
 
